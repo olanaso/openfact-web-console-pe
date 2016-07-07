28325d9282b0ceaca3b743defaae93e85d544d9e
shanitohl <shanohl.sist@gmail.com>
2016-07-07 16:35:40 -0500

    cambiando a tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { TopAreaComponent } from './shared/top-area';
+import {ViewEncapsulation} from '@angular/core';
+import { CenterAreaComponent } from './shared/center-area';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css','PatternFly/css/patternfly.css','PatternFly/css/patternfly-additions.css']
+  directives: [TopAreaComponent], 
+  styleUrls: ['app.component.css','PatternFly/css/patternfly.css','PatternFly/css/patternfly-additions.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/shared/top-area/top-area.component.ts
+++ src/app/shared/top-area/top-area.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopAreaComponent implements OnInit {
 
-  constructor() {}
+  constructor() {
+
+
+  }
 
   ngOnInit() {
   }

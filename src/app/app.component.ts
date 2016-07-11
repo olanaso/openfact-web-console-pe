8840988b62771b190dda9c3aa8300663febd88dc
erik <pavel.sist@gmail.com>
2016-07-11 17:50:42 -0500

    .

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { HeaderComponent } from './layout/header';
+import { SidebarComponent } from './layout/sidebar';
+import { ContainerComponent } from './layout/container';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css', '../styles/main.css'],
+  encapsulation: ViewEncapsulation.None,
+  directives: [HeaderComponent, SidebarComponent, ContainerComponent]
+})
+export class AppComponent {
+  title = 'app works!';
+}

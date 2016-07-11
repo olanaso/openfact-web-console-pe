9b14a4e22730bc9a06f781d61b0d47fd85e869d7
erik <pavel.sist@gmail.com>
2016-07-11 17:30:46 -0500

    .

--- src/app/documentos-electronicos/module-factura/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './module-factura.component';
-//export * from './module-factura.routes';
--- src/app/documentos-electronicos/module-factura/module-factura.routes.ts
+++
@@ -1,7 +0,0 @@
-    import { RouterConfig }          from '@angular/router';
-    import { ModuleFacturaComponent } from './module-factura.component';
-    
-    export const FacturaRoutes: RouterConfig = [
-      { path: 'factura',  component: ModuleFacturaComponent },
-     
-    ];

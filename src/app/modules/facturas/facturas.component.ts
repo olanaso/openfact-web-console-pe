99dd2c4c29830b46394e72a5364b48565e2b4b1a
erik <pavel.sist@gmail.com>
2016-07-13 16:49:11 -0500

    .

--- src/app/modules/facturas/facturas.component.ts
+++ src/app/modules/facturas/facturas.component.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {CORE_DIRECTIVES} from '@angular/common';
 import {DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { FacturaService } from '../../../app/services/facturas.service';
+import { Factura } from '../../../app/services/facturas';
 
 
 @Component({
   moduleId: module.id,
   selector: 'app-facturas',
   templateUrl: 'facturas.component.html',
-  styleUrls: ['facturas.component.css']
+  styleUrls: ['facturas.component.css'],
+  providers: [FacturaService],
+  directives: [DROPDOWN_DIRECTIVES, CORE_DIRECTIVES]
 
-  
 })
 // export class FacturasComponent implements OnInit {
 
@@ -20,30 +23,25 @@ import {DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 // })
 
 export class FacturasComponent implements OnInit {
-  public disabled:boolean = false;
-  public status:{isopen:boolean} = {isopen: false};
-  public items:Array<string> = ['The first choice!',
-    'And another choice for you.', 'but wait! A third!'];
- public ListaFacturas=[
-   {
-    "createdAt":1456399292790,
-    "isActive":true,
-    "name":"Hero 1"
-   },
-   {
-    "createdAt":1456399371220,
-    "isActive":true,
-    "name":"Hero 2"
-   },
-   {
-     "createdAt":1456399374548,
-     "isActive":true,
-     "name":"Hero 3"
-   }
- ];
-  constructor(private router: Router) { }
 
+
+
+  public disabled: boolean = false;
+  public status: { isopen: boolean } = { isopen: false };
+  public items: Array<string> = ['The first choice!',
+    'And another choice for you.', 'but wait! A third!'];
+    title = 'Tour of Heroes';
+  facturas: Factura[];
+  selectedFactura: Factura;
+  constructor(private router: Router, private facturaService: FacturaService) {
+  
+  }
+
+  getFacturas() {
+    this.facturaService.getFacturas().then(facturas => this.facturas = facturas);
+  }
   ngOnInit() {
+    this.getFacturas();
   }
 
   nuevo() {
@@ -66,16 +64,13 @@ export class FacturasComponent implements OnInit {
     this.router.navigate(link);
   }
 
-  public toggled(open:boolean):void {
+  public toggled(open: boolean): void {
     console.log('Dropdown is now: ', open);
   }
-  public toggleDropdown($event:MouseEvent):void {
+  public toggleDropdown($event: MouseEvent): void {
     $event.preventDefault();
     $event.stopPropagation();
     this.status.isopen = !this.status.isopen;
   }
-
-   
-
 
 }

9918e8986f36a7f924fa7077e86e787ac7431f20
shanitohl <shanohl.sist@gmail.com>
2016-08-08 09:48:33 -0500

    as

--- src/app/services/models/customer-model.ts
+++ src/app/services/models/customer-model.ts
@@ -5,13 +5,19 @@ import { Model } from './model';
 import { RestangularOpenfactService } from '../providers/restangular-openfact.service';
 import { RestangularService } from '../providers/restangular.service';
 
-export class CustomerModel extends Model {
+export class CustomerModel implements Model {
+    /*Restangular*/
+    restangular: RestangularService;
+
     id: string //ID DEL CLIENTE
     assignedIdentificationId: string // NUMERO DE DOCUMENTO.
     additionalAccountId: string //TIPO DE DOCUMENTO -- RUC O DNI
     registrationName: string // RAZON SOCIAL DE LA EMPRESA
     email: string;//CORREO ELECTRONICO DEL CLIENTE
-    // constructor(restangularOpenfactService: RestangularOpenfactService) {
-    //     super(restangularOpenfactService);
-    // }
+
+
+    /*Constructor*/
+    constructor(restangularService: RestangularService) {
+        this.restangular = restangularService;
+    }
 }

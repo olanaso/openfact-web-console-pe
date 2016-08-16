c8a893ca90cd6b2d9e06e5e270e62feffc9713e2
Alex Pariona <lx7pary@gmail.com>
2016-08-16 11:45:38 -0500

    ..

--- src/app/services/models/certificate-model.ts
+++ src/app/services/models/certificate-model.ts
@@ -1,24 +1,24 @@
 import { Model } from './model'
 import { Restangular } from '../restangular';
 
-export class CertifiedModel implements Model {
+export class CertificateModel implements Model {
 
   /*Restangular*/
   restangular:Restangular;
 
   /*Attributes*/
   public alias:string;//Alias del Certificado
-  public certificate:any;//Certificado
+  public certified:any;//Certificado
   public password:string;//password del certificado
   public passwordConfirmation:string;//IMPORTE TOTAL
   public validity:Date;//Fecha Vigencia
- 
+
   /*Constructor*/
   constructor(restangular:Restangular) {
     this.restangular = restangular;
   }
 
-  public clone(): CertifiedModel {
+  public clone(): CertificateModel {
     let copy = Object.assign({}, this);
     delete copy['restangular'];
     return copy;

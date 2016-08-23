a3f9a913491de3b258eb4938408d745edda078d6
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2016-08-23 11:13:50 -0500

    add total invoice type

--- src/app/services/models/document-model.ts
+++ src/app/services/models/document-model.ts
@@ -2,7 +2,11 @@ import {Model} from './model'
 import {Restangular} from '../restangular';
 import {Buildable, ObjectBuilder, ResponseToModel} from '../utils';
 
+export const INVOICE_TYPE = 'INVOICE_TYPE';
 export const ADDITIONAL_IDENTIFICATION_ID = 'ADDITIONAL_IDENTIFICATION_ID';
+export const ADDITIONAL_INFORMATION = 'ADDITIONAL_INFORMATION';
+export const TOTAL_TAX = 'TOTAL_TAX';
+export const TAX_REASON = 'TAX_REASON';
 
 export class DocumentModel extends Model implements Buildable {
 

33728535aa717fcf8b8293d83d24854853b8638f
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2016-10-11 12:48:11 -0500

    add bootstrap module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,24 +4,26 @@ import { FormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 
-import { SharedModule} from '../shared';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from '../shared';
 
 import { CreateOrganizationComponent } from './create-organization';
 import { OrganizationsComponent } from './organizations/organizations.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    AdminRoutingModule,
-    SharedModule
-  ],
-  declarations: [
-    CreateOrganizationComponent,
-    OrganizationsComponent
-  ],
-  providers: [
+    imports: [
+        CommonModule,
+        FormsModule,
+        AdminRoutingModule,
+        NgbModule.forRoot(),
+        SharedModule
+    ],
+    declarations: [
+        CreateOrganizationComponent,
+        OrganizationsComponent
+    ],
+    providers: [
 
-  ]
+    ]
 })
 export class AdminModule { }

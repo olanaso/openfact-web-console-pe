94e8941b323f9d76a9d969420419a952f61f3d25
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2016-11-11 17:17:54 -0500

    angular cli 19

--- openfact-web-console/src/main.ts
+++ openfact-web-console/src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import { KeycloakService } from "./app/keycloak.service";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+KeycloakService.init().then(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+}).catch(() => window.location.reload());
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { AdminModule } from './admin';
-import { SharedModule } from './shared';
-
-import { KeycloakService } from "./keycloak.service";
-import { KeycloakHttp } from "./keycloak.http";
 
 @NgModule({
   declarations: [
@@ -21,25 +12,9 @@ import { KeycloakHttp } from "./keycloak.http";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    NgbModule.forRoot(),
-    AdminModule,
-    SharedModule,
-    AppRoutingModule
+    HttpModule
   ],
-  providers: [
-    KeycloakService,
-    {
-      provide: Http,
-      useFactory:
-      (
-        backend: XHRBackend,
-        defaultOptions: RequestOptions,
-        keycloakService: KeycloakService
-      ) => new KeycloakHttp(backend, defaultOptions, keycloakService),
-      deps: [XHRBackend, RequestOptions, KeycloakService]
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

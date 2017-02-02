a46cd08f39af2d174d822a088451f1e31735fa59
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-02-02 08:44:35 -0500

    overview credit and debit note

--- src/app/organization/documents/credit-notes/credit-note-edit-overview/credit-note-edit-overview.component.ts
+++ src/app/organization/documents/credit-notes/credit-note-edit-overview/credit-note-edit-overview.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { AlertService } from './../../../../core/alert/alert.service';
+import { DataService } from './../../../../core/data/data.service';
+import { Document } from './../../../../core/model/document.model';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'of-credit-note-edit-overview',
   templateUrl: './credit-note-edit-overview.component.html',
   styleUrls: []
 })
-export class CreditNoteEditOverviewComponent implements OnInit {
+export class CreditNoteEditOverviewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  dataSubscription: Subscription;
+
+  document: Document;
+  documentJson: Observable<any>;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private dataService: DataService,
+    private alertService: AlertService) {
+  }
 
   ngOnInit() {
+    this.dataSubscription = this.activatedRoute.parent.data.subscribe(data => {
+      this.document = data['document'];
+      this.document.getJsonRepresentation().subscribe(data => {
+        this.documentJson = data;
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.dataSubscription.unsubscribe();
   }
 
 }

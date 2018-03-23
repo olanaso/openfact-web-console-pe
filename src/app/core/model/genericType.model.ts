d6c2c324246ddb4b328ba864f987f15b786e77d7
Carlos Feria <carlosthe19916@gmail.com>
2018-03-23 15:55:53 -0500

    first template

--- src/app/core/model/genericType.model.ts
+++
@@ -1,9 +0,0 @@
-export interface GenericType {
-    codigo?: string;
-    abreviatura?: string;
-    denominacion?: string;
-    grupo?: string;
-    length?: number;
-    afectaIgv?: boolean;
-    valor?: number;
-}

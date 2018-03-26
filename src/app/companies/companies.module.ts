7d4494890c9f43a6394acc9c8d66a09d76433981
Carlos Feria <carlosthe19916@gmail.com>
2018-03-26 17:15:21 -0500

    add search organizations

--- src/app/companies/companies.module.ts
+++ src/app/companies/companies.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { CompaniesComponent } from './companies.component';
 import { CompaniesRoutingModule } from './companies-routing.module';
 import { LoadingModule } from './../loading/loading.module';
@@ -7,6 +8,7 @@ import { LoadingModule } from './../loading/loading.module';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     CompaniesRoutingModule,
     LoadingModule
   ],

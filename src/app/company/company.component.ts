3d80e5140656576dd4142c650211523da89db698
Carlos Feria <carlosthe19916@gmail.com>
2018-03-27 10:02:43 -0500

    header ready

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,9 +1,9 @@
+import { KeycloakService } from './../keycloak-service/keycloak.service';
 import { Component, OnInit } from '@angular/core';
 import { NavigationItemConfig } from 'patternfly-ng/navigation';
 import { ContextItemConfig } from '../layout/vertical-navigation/context-item-config';
-
 import { User, UserService } from './../ngx-login-client';
-import { Company, CompanyService } from './../ngx-openfact/';
+import { Contexts, Context, Company, CompanyService } from './../ngx-openfact/';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -16,22 +16,32 @@ import 'rxjs/add/observable/forkJoin';
 })
 export class CompanyComponent implements OnInit {
 
+  loggedInUser: User;
+
   contextItems: ContextItemConfig[];
   navigationItems: NavigationItemConfig[];
 
   companies: Company[] = [];
 
-  private loggedInUser: User;
+  private context: Context;
   private subscriptions: Subscription[] = [];
 
   constructor(
     private userService: UserService,
-    private companyService: CompanyService
+    private companyService: CompanyService,
+    private contexts: Contexts,
+    private KeycloakService: KeycloakService
   ) {
     this.subscriptions.push(
       this.userService.loggedInUser.subscribe((val) => {
         this.loggedInUser = val;
         this.fetchCompanies();
+      })
+    );
+
+    this.subscriptions.push(
+      this.contexts.current.subscribe((val) => {
+        this.context = val;
       })
     );
   }
@@ -105,7 +115,7 @@ export class CompanyComponent implements OnInit {
     this.contextItems = this.companies.map((company) => {
       return {
         title: company.name,
-        url: '/_companies/' + company.id
+        url: '/_company/' + company.id
       };
     });
   }
@@ -116,6 +126,14 @@ export class CompanyComponent implements OnInit {
 
   onNavigation($event: NavigationItemConfig): void {
 
+  }
+
+  get contextLabel() {
+    return this.context.company.name;
+  }
+
+  logout() {
+    this.KeycloakService.logout();
   }
 
 }
--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CompanyComponent } from './company.component';
 import { CompanyRoutingModule } from './company-routing.module';
 import { VerticalNavigationModule } from './../layout/vertical-navigation/vertical-navigation.module';
 import { SidebarModule } from './sidebar/sidebar.module';
+import { NxgLoginModule } from './../ngx-login-client/ngx-login.module';
 
 @NgModule({
   imports: [
     CommonModule,
     CompanyRoutingModule,
+    BsDropdownModule,
+    NxgLoginModule,
     VerticalNavigationModule,
     SidebarModule
   ],
